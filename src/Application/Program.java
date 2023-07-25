beda3d1dde10132d04796c5761a3ad628d00422b
Jefferson Silva <jejefferson57@gmail.com>
2023-07-25 18:36:18 -0300

    Bad solution

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Room number");
+		System.out.print("Room number: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy):");
 		Date checkIn = sdf.parse(sc.next());// vai aparecer uma exceção, adicionei um throws
@@ -34,19 +34,16 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy):");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)){
-				System.out.println("Error in Reservation: check-out date must be after check-in date");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("reservation " + reservation);
-
-			}
+		
+				String error = reservation.updateDates(checkIn, checkOut);
+				if(error != null) {
+					System.out.println("Error in Reservation : " + error);
+				}
+				else {
+					System.out.println("reservation " + reservation);
+				}
 		}
+	
 
 		sc.close();
 	}
